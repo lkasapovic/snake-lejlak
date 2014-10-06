30d6d05243b67566e32cd8b0914a0392b41ce85a
Lejla Kaasapovic <lakergirl.lk@gmail.com>
2014-10-06 10:43:55 -0700

    video 8

--- public_html/js/snake.js
+++ public_html/js/snake.js
@@ -1,1 +1,17 @@
+var snake;
 
+var context;
+var screenWidth;
+var screenHeight;
+
+function gameInitialize() {
+    var canvas = document.getElementById("game-screen");
+}
+
+function gameLoop() {
+    
+}
+
+function gameDraw() {
+    
+}
