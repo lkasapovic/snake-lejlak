de096cb3315f17f6950ff284ae7925901f46b055
Lejla Kaasapovic <lakergirl.lk@gmail.com>
2014-10-07 09:20:15 -0700

    vid 10

--- public_html/js/snake.js
+++ public_html/js/snake.js
@@ -4,8 +4,18 @@ var context;
 var screenWidth;
 var screenHeight;
 
+gameInitialize();
+gameDraw();
+
 function gameInitialize() {
     var canvas = document.getElementById("game-screen");
+    context = canvas.getContext("2d");
+    
+    screenWidth = window.innerWidth;
+    screenHeight = window.innerHeight;
+    
+    canvas.width = screenWidth;
+    canvas.height = screenHeight;
 }
 
 function gameLoop() {
@@ -13,5 +23,6 @@ function gameLoop() {
 }
 
 function gameDraw() {
-    
+  context.fillStyle = "rgb(133, 247, 255)";
+  context.fillRect(0, 0, screenWidth, screenHeight);
 }
