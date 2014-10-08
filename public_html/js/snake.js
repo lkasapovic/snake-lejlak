8f12caac0957fa5428e040674dbf7f2191e5e3cf
Lejla Kaasapovic <lakergirl.lk@gmail.com>
2014-10-08 09:19:52 -0700

    19

--- public_html/js/snake.js
+++ public_html/js/snake.js
@@ -1,11 +1,15 @@
 var snake;
+var snakeLength;
+var  snakeSize;
 
 var context;
 var screenWidth;
 var screenHeight;
 
 gameInitialize();
+snakeInitialize();
 gameDraw();
+snakeDraw();
 
 function gameInitialize() {
     var canvas = document.getElementById("game-screen");
@@ -25,4 +29,29 @@ function gameLoop() {
 function gameDraw() {
   context.fillStyle = "rgb(133, 247, 255)";
   context.fillRect(0, 0, screenWidth, screenHeight);
+}
+
+function snakeInitialize() {
+  snake = [];
+  snakeLengh: 15;
+  snakeSize: 20;
+  
+  for( var index = 0; index < snakeLength; index++) {
+     snake.push( {
+         x: index,
+         y: 0
+     }); 
+  }
+}
+
+function snakeDraw() {
+    
+}
+
+function snakeUpdate() {
+    for( var index = 0; index < 0; index++) {
+        context.fillStyle= "white";
+        context.fillRect(snake[index].x * snakeSize, snake [index].y * snakeSize, snakeSize, snakeSize);
+        
+    }
 }
